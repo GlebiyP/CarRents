edb97aeb68426a1cafc4e9fb6badc1c7eb8d894f
Gleb <gleb.0997@gmail.com>
2021-09-26 21:12:12 +0300

    Update Renter.cs

--- Models/Renter.cs
+++ Models/Renter.cs
@@ -16,11 +16,11 @@ namespace CarRents.Models
         public int RenterID { get; set; }
 
         [Display(Name = "Name")]
-        [Required(ErrorMessage = "The field cannot be empty!")]
+        [Required(ErrorMessage = "This field is required.")]
         public string RenterName { get; set; }
 
         [Display(Name = "City")]
-        [Required(ErrorMessage = "The field cannot be empty!")]
+        [Required(ErrorMessage = "This field is required.")]
         public string City { get; set; }
 
         [Display(Name = "Birth year")]
@@ -28,7 +28,7 @@ namespace CarRents.Models
         public int BirthYear { get; set; }
 
         [Display(Name = "Email")]
-        [Required(ErrorMessage = "The field cannot be empty")]
+        [Required(ErrorMessage = "This field is required.")]
         [RegularExpression(@"([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$", ErrorMessage = "Incorrect data!")]
         public string Email { get; set; }
 
