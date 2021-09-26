4ee76441aeb48ef74c79b97e96b2a6166bf8be3d
Gleb <gleb.0997@gmail.com>
2021-09-26 21:12:14 +0300

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -54,7 +54,7 @@ namespace CarRents
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Cars}/{action=Index}/{id?}");
             });
         }
     }
