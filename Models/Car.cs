92a4770b12d0376fbab9f7bf74c453ff9fdc8bcb
Gleb <gleb.0997@gmail.com>
2021-09-26 21:12:06 +0300

    Update Car.cs

--- Models/Car.cs
+++ Models/Car.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Http;
 
 namespace CarRents.Models
 {
@@ -19,25 +21,32 @@ namespace CarRents.Models
         public int BrandID { get; set; }
 
         [Display(Name = "Release year")]
-        [Required(ErrorMessage = "The field cannot be empty!")]
+        [Required(ErrorMessage = "This field is required.")]
         [Range(1940, 2021, ErrorMessage = "Incorrect data!")]
         public string CarYear { get; set; }
 
         [Display(Name = "Body")]
-        [Required(ErrorMessage = "The field cannot be empty!")]
+        [Required(ErrorMessage = "This field is required.")]
         public string Body { get; set; }
 
         [Display(Name = "Model")]
-        [Required(ErrorMessage = "The field cannot be empty!")]
+        [Required(ErrorMessage = "This field is required.")]
         public string Model { get; set; }
 
         [Display(Name = "Color")]
-        [Required(ErrorMessage = "The field cannot be empty!")]
+        [Required(ErrorMessage = "This field is required.")]
         public string Color { get; set; }
 
         [Display(Name = "Price per day")]
         [Range(0, 1000000, ErrorMessage = "Incorrect data!")]
         public double Price { get; set; }
+
+        [Display(Name = "Image")]
+        public string ImagePath { get; set; }
+
+        [NotMapped]
+        [Display(Name = "Upload file")]
+        public IFormFile ImageFile { get; set; }
 
         [Display(Name = "Brand")]
         public virtual Brand Brand { get; set; }
